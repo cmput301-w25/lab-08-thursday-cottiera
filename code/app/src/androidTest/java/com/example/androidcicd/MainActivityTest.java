de1db25a478d854c59fc91b811ba58977b75f4d3
Alastair Cottier <cottiera@shaw.ca>
2025-03-07 14:11:32 -0700

    :adhesive_bandage: fix tests

--- code/app/src/androidTest/java/com/example/androidcicd/MainActivityTest.java
+++ code/app/src/androidTest/java/com/example/androidcicd/MainActivityTest.java
@@ -10,6 +10,7 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import android.os.SystemClock;
 import android.util.Log;
 
 import androidx.test.espresso.ViewInteraction;
@@ -66,6 +67,9 @@ public class MainActivityTest {
             movie.setId(docRef.getId());
             docRef.set(movie);
         }
+
+        // Delay
+        SystemClock.sleep(2000);
     }
 
     @Test
